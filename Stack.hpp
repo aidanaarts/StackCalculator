b9854093b346fe1614849c1693e3f059b3930df4
aidanaarts <aidanaarts@gmail.com>
2019-04-26 12:04:09 -0600

    Create Stack.hpp

---
+++ Stack.hpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+template<class T>
+struct node {
+    T data;
+    node<T> *nxt;
+};
+
+template<class T>
+struct myStack {
+    node<T> *head;
+    myStack() {
+        head = NULL;
+    }
+    void push(T x) {
+        node<T> *n = new node<T>();
+        n->data = x;
+        n->nxt = head;
+        head = n;
+    }
+
+    T pop(int *empty) {
+        if (head == NULL) {
+            *empty = 1;
+            return 0;
+        }
+        *empty = 0;
+        node<T> *tmp = head;
+        head = head->nxt;
+        T x = tmp->data;
+        delete (tmp);
+        return x;
+    }
+
+    char top() {
+        if (head == NULL) {
+            return 0;
+        }
+        return head->data;
+    }
+};
